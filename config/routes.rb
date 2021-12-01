1fa8ec8854c5b8fb956d02cb741b5bd64b44de6a
SaayaShimada <93375049+saaya0@users.noreply.github.com>
2021-12-01 15:05:28 +0900

    bookers2-debug

---
+++ config/routes.rb
@@ -0,0 +1,10 @@
+Rails.application.routes.draw do
+ devise_for :users
+ get 'home/about' => 'homes#about'
+ root 'homes#top'
+  resources :users,only: [:show,:index,:edit,:update]
+  resources :books
+
+
+
+end
